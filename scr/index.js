2e63b762bc7a517d37a1f43222fdec32491b8313
gabsam16 <gabrieldaluz16122004@gmail.com>
2025-01-03 23:21:50 -0400

    segundos cambios

--- scr/index.js
+++ scr/index.js
@@ -5,7 +5,7 @@ const app = express()
 
 const port = 3005
 app.use(cors({origin:'*'}))// cors
-app.use(express.jsopn({limit:'50mb'}))
+app.use(express.json({limit:'50mb'}))
 app.use(express.urlencoded({extended:false}))
 
 app.listen(port,function(){
@@ -13,7 +13,7 @@ app.listen(port,function(){
 })
 
 app.get('/',(req,res)=>{
-    console.log('mi primer endpoint')
+    console.log('Mi primer endpoint')
     res.status(200).send('Mi primer chiste');
 
 });
