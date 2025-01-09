86006d656835fda7c1bdd7d8b39686644352b74b
Zarah <zvroa.22@est.ucab.edu.ve>
2025-01-08 20:17:55 -0400

    FIX: corregidos url de endpoints

--- src/index.js
+++ src/index.js
@@ -322,8 +322,46 @@ app.delete('/chiste/:id', async (req, res) => {
     }
 });
 
+
 //REQUERIMIENTO 5: Obtener chiste por su ID
-app.get('/chistes/:id', async (req, res) => { 
+/**
+ * @swagger
+ * /chiste/{id}:
+ *   get:
+ *     summary: Obtiene un chiste por su ID
+ *     description: Devuelve un chiste específico basado en su ID.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID del chiste a obtener
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Chiste obtenido exitosamente
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 _id:
+ *                   type: string
+ *                 texto:
+ *                   type: string
+ *                 autor:
+ *                   type: string
+ *                 puntaje:
+ *                   type: integer
+ *                 categoria:
+ *                   type: string
+ *       404:
+ *         description: Chiste no encontrado
+ *       500:
+ *         description: Error interno del servidor
+ */
+
+app.get('/chiste/:id', async (req, res) => { 
     try { 
         const chiste = await Chiste.findById(req.params.id); 
         if (!chiste) { 
@@ -336,7 +374,37 @@ app.get('/chistes/:id', async (req, res) => {
 });
 
 //REQUERIMIENTO 6: Obtener la cantidad de chistes que hay en la base de datos por su categoría
-app.get('/chistes/categoria/:categoria', async (req, res) => { 
+/**
+ * @swagger
+ * /chiste/categoria/{categoria}:
+ *   get:
+ *     summary: Obtiene la cantidad de chistes por categoría
+ *     description: Devuelve la cantidad de chistes en la base de datos según la categoría especificada.
+ *     parameters:
+ *       - in: path
+ *         name: categoria
+ *         required: true
+ *         description: Categoría de los chistes a contar
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Cantidad de chistes obtenida exitosamente
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 categoria:
+ *                   type: string
+ *                 cantidad:
+ *                   type: integer
+ *       404:
+ *         description: No se encontraron chistes para esta categoría
+ *       500:
+ *         description: Error interno del servidor
+ */
+app.get('/chiste/categoria/:categoria', async (req, res) => { 
     const categoria = req.params.categoria; 
     try { 
         const count = await Chiste.countDocuments({ categoria }); 
